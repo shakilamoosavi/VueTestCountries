3cc945cdc3b53dc8a6bd8ab3a55eb61db02937e4
shakilamoosavi <shakilamoosavi@yahoo.com>
2022-07-27 14:48:41 +0430

    fix nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: 'static',
   router: {
-    base: '/restCountry'
+    base: '/VueTestCountries'
   },
   head: {
     title: 'test',
